339ae8d453e7aa8d15ef792bc96493c191ad4727
vicomte <vicomte.d.v@gmail.com>
2014-08-04 13:03:00 +0000

    Database model included

---
+++ red/models.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from red import db
+
+class Websites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(40), unique=True)
+    url = db.Column(db.String(120), unique=False)
+    created = db.Column(db.DateTime)
+
+    def __init__(self, label, url):
+        self.label = label
+        self.url = url
+        self.created = datetime.utcnow()
+
+    def __repr__(self):
+        return '<Website %r>' % self.label
+
+    def __str__(self):
+        return '%s|%s|%s' % (self.label, self.url, str(self.created))
+
