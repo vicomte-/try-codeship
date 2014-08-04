acb8459db34d2c8070dae78eb03e39507229ac94
vicomte <vicomte.d.v@gmail.com>
2014-08-04 12:17:51 +0000

    helpers.py added after accidental removal

---
+++ red/helpers.py
@@ -0,0 +1,46 @@
+import os
+from datetime import datetime, timedelta
+from flask import session
+from collections import deque
+
+__author__ = 'Surfer'
+
+
+def brackify(tag):
+    return '<%s>' % tag
+
+
+def taggify(html, tags):
+    balanced_tags = deque([html])
+    for tag in tags:
+        balanced_tags.appendleft(brackify(tag))
+        balanced_tags.append(brackify('/%s' % tag))
+    return ''.join(balanced_tags)
+
+
+def mark_as_preformatted(html):
+    return taggify(html, ['pre', 'code'])
+
+
+def calc_expiration():
+    timeout = os.environ.get('SESSION_TIMEOUT')
+    if not timeout:
+        timeout = 100
+    else:
+        timeout = int(timeout)
+    return datetime.now() + timedelta(seconds=timeout)
+
+
+def check_logged_in():
+    if session.get('logged_in'):
+        if datetime.now() <= session['expiration']:
+            session['expiration'] = calc_expiration()
+            return True
+    return False
+
+
+def set_default(check, default):
+    if check:
+        return check
+    else:
+        return default
